b934e44c78f1b089af71eb22857853adf187ea71
blurry-mood <assis.ayoub@gmail.com>
2021-06-28 16:40:18 +0100

    Eliminate file names from import statement

--- deepblocks/__init__.py
+++ deepblocks/__init__.py
@@ -1,4 +1,3 @@
-from .CNN.FlipConv2d import FlipConv2d
-from .CNN.FReLU import FReLU
-from .CNN.SE import SE
-from .CNN.InputAware import ConvBlock as InputAwareConv2d
+from .CNN import *
+
+from .loss import *
