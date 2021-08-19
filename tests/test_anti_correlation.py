44908e464d92b1f84cf9a7920cf42d3f58c6f12d
blurry-mood <assis.ayoub@gmail.com>
2021-08-19 14:06:59 +0100

    decrease the value of lambda

--- tests/test_anti_correlation.py
+++ tests/test_anti_correlation.py
@@ -19,21 +19,21 @@ def test_break_symmetry():
     opt = torch.optim.Adam([{'params': param}], lr=1e-1)
     x = torch.rand(10, 3)
     for _ in range(100):
-        opt.zero_grad()
         y = x@param
         y = y.mean()
+        opt.zero_grad()
         y.backward()
         opt.step()
         assert (param[1:]-param[:-1]).abs().sum() < 1e-1
 
-    anti = AntiCorrelation(p=1., lmd=1.)
+    anti = AntiCorrelation(p=1., lmd=.01)
     param = nn.Parameter(torch.ones(3, 4))
     opt = torch.optim.Adam([{'params': param}], lr=1e-1)
     for _ in range(100):
-        opt.zero_grad()
         y = x @ param
         loss = anti([y])
         y = y.mean() + loss
+        opt.zero_grad()
         y.backward()
         opt.step()
     assert (param[1:]-param[:-1]).abs().mean() > 1e-1
