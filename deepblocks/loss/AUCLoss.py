076829607afc20907f43cf3fe44f1e6c80a42da6
blurry-mood <assis.ayoub@gmail.com>
2021-08-28 15:55:18 +0100

    process case when batch has positive/or negative samples only for aucloss

--- deepblocks/loss/AUCLoss.py
+++ deepblocks/loss/AUCLoss.py
@@ -1,11 +1,12 @@
 import torch
 from torch import Tensor
+from torch.nn.functional import binary_cross_entropy
 
 from ._Loss import _Loss
 
 
 @torch.jit.script
-def _binary_loss(x: torch.Tensor, y: torch.Tensor, gamma: float, p: float):
+def _binary_loss(x: torch.Tensor, y: torch.Tensor, gamma: float, p: float, skip: bool):
     x = torch.sigmoid(x)
     y = y.unsqueeze(1)     # make it broadcastable
 
@@ -13,11 +14,16 @@ def _binary_loss(x: torch.Tensor, y: torch.Tensor, gamma: float, p: float):
     pos, neg = [y == i for i in [1, 0]]
     # compute masked values
     pos, neg = [torch.masked_select(x, _x) for _x in [pos, neg]]
+    if pos.shape == (0,) or neg.shape == (0,):
+        return torch.tensor(1., dtype=torch.float32, requires_grad=True, device=y.device) if skip else binary_cross_entropy(x, y.float(), reduction='none')
+
     # compute difference
     diff = pos.unsqueeze(0) - neg.unsqueeze(1) - gamma
     # compute mask for diff<gamma
     mask = diff < 0
     diff = torch.masked_select(diff, mask)
+    if diff.shape == (0, ):
+        return torch.tensor(1., dtype=torch.float32, requires_grad=True, device=y.device) if skip else binary_cross_entropy(x, y.float(), reduction='none')
     # compute loss
     loss = (-diff).pow(p)
     return loss
@@ -25,15 +31,21 @@ def _binary_loss(x: torch.Tensor, y: torch.Tensor, gamma: float, p: float):
 
 class AUCLoss(_Loss):
     """ This loss is an approximation to WilcoxonMann-Whitney (WMW) statistic. 
-        It's used to directly maximize the Area Under the Curve (AUC) metric.
+        It's used to directly maximize the Area Under the Curve (AUC) metric. 
+        Note that each batch must have both positive and negative samples and 
+        at least a pair of positive & negative samples must satisfy the constraint (pos_i - neg_j < gamma) 
+        to compute this loss, otherwise, the weights aren't updated if `skip==True` else the BCELoss is computed for that batch.
         Only the binary classification case is supported by this implementation.
-        
+
         For more details check: https://www.aaai.org/Papers/ICML/2003/ICML03-110.pdf
 
     Args:
         - gamma (Float, Default=0.3): denotes the tolerance in the difference between positive and negative samples, 
-                                    if not exceeded that pair doesn't contribute to the loss.
+                                    if not exceeded that pair doesn't contribute to the loss. 
+                                    The larger the `gamma`, the more the model enlarges the gap between positive and negative predictions
         - p (Float, Default=2.0): denotes the exponent used for the aforementioned difference.
+        - skip (Boolean, Default=False): specifies whether to skip, if true, the current weight update step or to use the BCELoss function instead if false, 
+                                     when the current batch doesn't have both positive and negative samples or none of the pairs satisfy (pos_i - neg_j < gamma).
         - reduction(String, Default='mean'): specifies the reduction to apply to the output: 'none' | 'mean' | 'sum'. 
                                     'none': no reduction will be applied, 
                                     'mean': the sum of the output will be divided by the number of elements in the output, 
@@ -45,8 +57,8 @@ class AUCLoss(_Loss):
 
     """
 
-    def __init__(self, gamma: float = 0.3, p: float = 2, reduction: str = 'mean'):
-        super(AUCLoss, self,).__init__(gamma, p, reduction)
+    def __init__(self, gamma: float = 0.3, p: float = 2, skip: bool = False, reduction: str = 'mean'):
+        super(AUCLoss, self,).__init__(gamma, p, skip, reduction)
         if not (0 < gamma <= 1):
             raise ValueError(
                 f'The value of gamma={gamma} must be in (0, 1]')
@@ -57,9 +69,9 @@ class AUCLoss(_Loss):
         self.loss = _binary_loss
 
     def forward(self, x: Tensor, y: Tensor):
-        loss = self.loss(x, y, self.gamma, self.p)
-        if self.reduction  == 'mean':
+        loss = self.loss(x, y, self.gamma, self.p, self.skip)
+        if self.reduction == 'mean':
             return loss.mean()
-        elif self.reduction =='sum':
+        elif self.reduction == 'sum':
             return loss.sum()
         return loss
--- tests/test_auc_loss.py
+++ tests/test_auc_loss.py
@@ -16,14 +16,14 @@ def test_consistency():
     auc = AUCLoss(gamma=0.1, reduction='mean')
     auroc = AUROC(pos_label=1, compute_on_step=True)
     opt = torch.optim.SGD([x], lr=3)
-    
+
     _loss, _metric = 1e10, -1e10
     for _ in range(100):
         loss = auc(x, y)
         opt.zero_grad()
         loss.backward()
         opt.step()
-        
+
         metric = auroc(torch.sigmoid(x), y.unsqueeze(1))
 
         assert _loss > loss.item()
@@ -31,3 +31,10 @@ def test_consistency():
 
         _loss = loss.item()
         _metric = metric.item()
+
+
+def test_skip():
+    auc = AUCLoss(gamma=0.1, skip=True, reduction='none')
+    x = torch.tensor([10, .3, 1, 1]).view(-1, 1)
+    y = torch.tensor([1, 1, 1, 1])
+    assert auc(x, y).item() == 1.0
