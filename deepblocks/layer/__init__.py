702ede786363aa80243864931cbee6e125851c56
blurry-mood <assis.ayoub@gmail.com>
2021-10-30 13:38:31 +0300

    add `PatchConv2d` to layers

--- deepblocks/layer/__init__.py
+++ deepblocks/layer/__init__.py
@@ -1,9 +1,11 @@
 r""" **Layer** implements useful PyTorch layers that can be easily incorporated within every model.
 
 It contains:
+    - FlipConv2d (Special Conv2d layer that's almost invariant to image flipping) for Convnets,
+    - PatchConv2d (Special Conv2d layer that computes L2 distance between image patches and kernel) for Convnets,
+
     - SE (Squeeze-Excitation) for Convnets,
     - DenseBlock (implemented in DenseNet) for Convnets,
-    - FlipConv2d (Special Conv2d layer that's almost invariant to image flipping) for Convnets,
     - MultiHeadAttention implemented in the transformers paper (Attention is all you need).
     - MultiHeadAttentionV2 is a modified version.
     - ConvMixerLayer (implemented in ConvMixer) for Convnets
@@ -11,5 +13,6 @@ It contains:
 from .FlipConv2d import FlipConv2d
 from .SE import SE
 from .MultiHeadAttention import MultiHeadAttention, MultiHeadAttentionV2
-from .DenseBlock import DenseBlock
 from .ConvMixerLayer import ConvMixerLayer
+from .PatchConv2d import PatchConv2d
+from .DenseBlock import DenseBlock
